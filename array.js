a0e1baff0b715f83b7f6e286f121ace42af4375e
Suman Bandita <sumanbandita@gmail.com>
2018-07-25 14:26:44 -0400

    test

--- array.js
+++ array.js
@@ -1,61 +1,77 @@
+'use strict';
 const mem = require('./memory');
 const memory = new mem();
 
 class Array {
-	constructor() {
-		this.length = 0;
-		this._capacity = 0;
-		this.ptr = memory.allocate(this.length);
-	}
-
-	push(value) {
-		if (this.length >= this._capacity) {
-			this._resize((this.length + 1) * Array.SIZE_RATIO);
+  constructor() {
+    this.length = 0;
+    this._capacity = 0;
+    this.ptr = memory.allocate(this.length);		
+  }
+	
+  get(index){
+    if(index < 0 || index >= this.length){
+			throw new Error('index error');			
 		}
+		return memory.get(this.ptr + index);    
+  }
 
-		memory.set(this.ptr + this.length, value);
-		this.length++;
-	}
+  push(value) {
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
 
-	pop() {
-		//get our length
-		if (this.length === 0) {
-			throw new Error('empty array')
-		}
-		const value = memory.get(this.ptr + this.length - 1);
-		this.length--;
-		return value;
-		//empty out the last index
-		//return
-	}
+    memory.set(this.ptr + this.length, value);
+    this.length++;
+  }
 
-	_resize(size) {
-		const oldPtr = this.ptr;
-		this.ptr = memory.allocate(size);
-		if (this.ptr === null) {
-			throw new Error('out of memory');
-		}
-		memory.copy(this.ptr, oldPtr, this.length);
-		memory.free(oldPtr);
-		this._capacity = size;
-	}
+  pop() {
+    //get our length
+    if (this.length === 0) {
+      throw new Error('empty array');
+    }
+    const value = memory.get(this.ptr + this.length - 1);
+    this.length--;
+    return value;
+    //empty out the last index
+    //return
+  }
+
+  _resize(size) {
+    const oldPtr = this.ptr;
+    this.ptr = memory.allocate(size);
+    if (this.ptr === null) {
+      throw new Error('out of memory');
+    }
+    memory.copy(this.ptr, oldPtr, this.length);
+    memory.free(oldPtr);
+    this._capacity = size;
+  }
+	
+	
+	
 }
 
 function main(){
 
-    Array.SIZE_RATIO = 3;
+  Array.SIZE_RATIO = 3;
 
-    //create an instance of the array class
-    let arr = new Array();
+  //create an instance of the array class
+  let arr = new Array();
 
-    //add an item to the array
-    arr.push(3);
-    arr.push(5);
-    arr.push(5);
-    arr.push('lkasjdf');
-    arr.pop();
+  //add an item to the array
+  arr.push(3);
+  arr.push(5);
+	arr.push(5);
+	arr.push(3);
+  arr.push(15);
+  arr.push(50);
+  arr.push('lkasjdf');
+  arr.pop();
+  console.log(arr.get(5));
+	
 
-    console.log(arr);
+  //console.log(arr);
 }
 
 main();
