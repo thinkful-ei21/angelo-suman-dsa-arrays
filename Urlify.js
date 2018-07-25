9811249c8e59c7f6c9185cb33fc3d18f5dc8d0ba
Suman Bandita <sumanbandita@gmail.com>
2018-07-25 18:00:05 -0400

    2d array

--- Urlify.js
+++ Urlify.js
@@ -51,16 +51,74 @@
 
 // mergeArray(arr1,arr2);
 /************************************************** */
-let str = 'Battle of the Vowels: Hawaii vs. Grozny';
-let charToRemove = {a: 'a',e: 'e',i: 'i',o: 'o',u: 'u'};
+// let str = 'Battle of the Vowels: Hawaii vs. Grozny';
+// let charToRemove = 'aeiou';
 
-function removeChar(str,charToRemove){
-  let newArr =[];
-  for(let i= 0; i< str.length;i++){
-    if(!(str.substring(i,i+1) in charToRemove)){     
-      newArr.push(str.substring(i,i+1));
-    }
-  }  
-  console.log(newArr.join(''));
+// function removeChar(str,charToRemove){
+//   let newStr = '';
+//   let found = false;
+//   for(let i= 0; i< str.length;i++){
+//     for(let j=0;j< charToRemove.length;j++){
+//       if(str[i] === charToRemove[j]){             
+//         found = true;        
+//       }
+//     }    
+//     if(found === false){
+//       newStr += str[i];      
+//     }
+//     found = false;
+//   }    
+//   console.log(newStr);
+// }
+// removeChar(str.toLowerCase(),charToRemove);
+
+/************************************************** */
+
+// let input = [1,3,9,4];
+// function product(input){
+//   let output = [1,1,1,1];
+//   for(let i=0; i< input.length;i++){    
+//     for(let j=0; j< input.length;j++){
+//       if(i !== j){
+//         output[i] = output[i] * input[j];
+//       }
+//     } 
+//   }
+//   console.log(output);
+// }
+// product(input);
+
+/************************************************** */
+
+// Pcik which ith and jth column would be made zero
+let input = [[1,0,1,1,0],
+  [0,1,1,1,0],
+  [1,1,1,1,1],
+  [1,0,1,1,1],
+  [1,1,1,1,1]];
+let ith =[];
+let jth =[];
+let output = [];
+
+for(let i=0; i<input.length; i++){  
+  output.push([...input[i]]);
+  for(let j=0;j< input[i].length;j++){    
+    if(input[i][j] === 0){
+      ith.push(i);
+      jth.push(j);
+    }       
+  }
 }
-removeChar(str,charToRemove);
+//console.log(ith,jth);
+console.log(output);
+for( let i=0; i< ith.length; i++){
+  for(let j=0; j< jth.length;j++){
+    if(ith.includes(i) || jth.includes(j)){
+      output[i][j] = 0;
+    }else{
+      output[i][j] = 1;
+    }
+  }
+}
+
+console.log(output);
